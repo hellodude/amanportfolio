11789e8fa660199c80411a68882030eb60807d56
hellodude <vaman852@gmail.com>
2020-05-05 01:32:37 +0530

    Updates

---
+++ precache-manifest.0a958d770a905ed13dc437ceab65fba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59925a3647cfe68ff164c8ecebb1157f",
+    "url": "/amanportfolio/index.html"
+  },
+  {
+    "revision": "eee8dac805771772e5bc",
+    "url": "/amanportfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "203fe0ee5bc65d871e5d",
+    "url": "/amanportfolio/static/js/2.1d434619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amanportfolio/static/js/2.1d434619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee8dac805771772e5bc",
+    "url": "/amanportfolio/static/js/main.aa51f92d.chunk.js"
+  },
+  {
+    "revision": "b1bfec20eaae9edc4172",
+    "url": "/amanportfolio/static/js/runtime-main.03b24dd2.js"
+  }
+]);
